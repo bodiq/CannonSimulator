4a81e0f5bd2b5e15e5876beb04ec61ad8ab82fb3
bodiq <bodia.baran.o@gmail.com>
2025-01-02 20:29:56 +0200

    Added cannon movement and rotation

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -1,19 +1,26 @@
 using System;
+using DefaultNamespace;
 using UnityEngine;
 
 public class Cannon : MonoBehaviour
 {
     [SerializeField] private Transform cameraSpot;
+    
+    [SerializeField] private Transform mainConstruction;
+    [SerializeField] private Transform cannonPipe;
 
     private CannonCameraSpot _cannonCameraSpot;
+    private CannonMovement _cannonMovement;
 
     private void Start()
     {
         _cannonCameraSpot = new CannonCameraSpot(Camera.main, cameraSpot);
+        _cannonMovement = new CannonMovement(mainConstruction, cannonPipe);
     }
 
     private void Update()
     {
         _cannonCameraSpot.SetCannonCamera();
+        _cannonMovement.MoveCannon();
     }
 }
---
+++ Assets/Scripts/CannonMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class CannonMovement
+    {
+        private Transform _mainConstruction;
+        private Transform _firePipe;
+
+
+        public CannonMovement(Transform mainConstruction, Transform firePipe)
+        {
+            _mainConstruction = mainConstruction;
+            _firePipe = firePipe;
+        }
+
+        public void MoveCannon()
+        {
+            MoveHorizontal();
+            MoveVertical();
+        }
+
+        private void MoveVertical()
+        {
+            var rotation = Input.GetAxis("Vertical") * Time.deltaTime * 100f;
+            _firePipe.rotation = Quaternion.Euler( _firePipe.rotation.x,  _firePipe.rotation.y,  _firePipe.rotation.z + rotation);
+        }
+
+        private void MoveHorizontal()
+        {
+            var movement = Input.GetAxis("Horizontal") * Time.deltaTime * 100f;
+            _mainConstruction.rotation = Quaternion.Euler(_mainConstruction.rotation.x, _mainConstruction.rotation.y + movement, _mainConstruction.rotation.z);
+        }
+    }
+}
